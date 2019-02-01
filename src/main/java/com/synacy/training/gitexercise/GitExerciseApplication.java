2261b78f6777a0dd1b535a072373550fff568020
JR Comision <catalino.comision@synacy.com>
2019-02-01 10:35:33 +0800

    Create spring boot application using initializer
    Dependencies were Data JPA, Thymeleaf, Web, Devtools, H2

---
+++ src/main/java/com/synacy/training/gitexercise/GitExerciseApplication.java
@@ -0,0 +1,14 @@
+package com.synacy.training.gitexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExerciseApplication.class, args);
+	}
+
+}
+
