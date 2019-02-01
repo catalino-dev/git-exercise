6a2c2f1006febbd778603170e6b44e9aa97fa71c
JR Comision <catalino.comision@synacy.com>
2019-02-01 10:53:14 +0800

    Add method calculation for subtraction received from subtract operator

--- src/main/java/com/synacy/training/gitexercise/calculator/CalculateMethod.java
+++ src/main/java/com/synacy/training/gitexercise/calculator/CalculateMethod.java
@@ -10,6 +10,14 @@ public enum CalculateMethod {
             double b = Double.valueOf(args[1]);
             return String.valueOf(a + b);
         }
+    },
+    SUBTRACT {
+        @Override
+        public String calculate(String[] args) {
+            double a = Double.valueOf(args[0]);
+            double b = Double.valueOf(args[1]);
+            return String.valueOf(a - b);
+        }
     };
 
     public abstract String calculate(String[] args);
