e668fec3785b3f4158ec2e40da98ec93cd335322
JR Comision <catalino.comision@gmail.com>
2019-02-04 12:13:51 +0800

    Add implementation for exponential operation to the power of 2

--- src/main/java/com/synacy/training/gitexercise/calculator/CalculateMethod.java
+++ src/main/java/com/synacy/training/gitexercise/calculator/CalculateMethod.java
@@ -34,6 +34,14 @@ public enum CalculateMethod {
             double b = Double.valueOf(args[1]);
             return String.valueOf(a / b);
         }
+    },
+    POWER_OF_2 {
+        @Override
+        public String calculate(String[] args) {
+            double a = Double.valueOf(args[0]);
+            double b = Double.valueOf(args[1]);
+            return String.valueOf(Math.pow(a, b));
+        }
     };
 
     public abstract String calculate(String[] args);
