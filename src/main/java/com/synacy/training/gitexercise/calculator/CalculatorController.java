b6a010399380ba05487c695608b5b7698726fe19
fctmolina <francis.molina@synacy.com>
2019-02-04 11:07:49 +0800

    Implement null check of parameters for COSINE operator in CalculatorController

--- src/main/java/com/synacy/training/gitexercise/calculator/CalculatorController.java
+++ src/main/java/com/synacy/training/gitexercise/calculator/CalculatorController.java
@@ -44,6 +44,35 @@ public class CalculatorController {
                     }
                 }
             }
+            else if (operator.equals("COSINE")) {
+                if (value1 != null && value2 == null) {
+                    CalculateMethod method = CalculateMethod.of(operator);
+                    String[] parameters = new String[] {value1, "0"};
+
+                    if (method != null) {
+                        String result = method.calculate(parameters);
+                        model.addAttribute("result", result);
+                    }
+                }
+                else if (value1 == null && value2 != null) {
+                    CalculateMethod method = CalculateMethod.of(operator);
+                    String[] parameters = new String[] {"0", value2};
+
+                    if (method != null) {
+                        String result = method.calculate(parameters);
+                        model.addAttribute("result", result);
+                    }
+                }
+                else {
+                    CalculateMethod method = CalculateMethod.of(operator);
+                    String[] parameters = new String[] {value1, value2};
+
+                    if (method != null) {
+                        String result = method.calculate(parameters);
+                        model.addAttribute("result", result);
+                    }
+                }
+            }
 
             CalculateMethod method = CalculateMethod.of(operator);
             String[] parameters = new String[] { value1, value2 };
